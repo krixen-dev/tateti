9563dec5bdca74cba70d4638dac99e1ee04128f6
Antonio <krixen.dev@gmail.com>
2023-09-19 00:30:50 -0300

    agrega lógica player2, bloquear inputs
    se agregó función para registrar los clicks del player2 además una vez que el usuario elige un cuadro bloquea los inputs para prevenir ue siga tocando los cuadros y además para que empieze el turno del otro jugador

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const player1 = document.getElementById("form__name_player1");
 const player2 = document.getElementById("form__name_player2");
 const panel = document.getElementById("panel");
 const $squares = panel.querySelectorAll(".square");
+let flagTurn = false;
 let sequencePlayer1 = [];
 let sequencePlayer2 = [];
 
@@ -15,19 +16,48 @@ btnNext.onclick = function () {
 btnPlayGame.onclick = function () {
   hideElement(document.getElementById("form"));
   showElement(document.getElementById("game"));
-  turnPlayer1();
+  manageRound();
 }
 
 
+
+
+
+function manageRound() {
+  if (!flagTurn) {
+    turnPlayer1();
+    console.log("turno player1");
+  }else{
+    turnPlayer2();
+    console.log("turno player2");
+  }
+}
 
 function turnPlayer1() {
   $squares.forEach(function ($square) {
     $square.onclick = function () {
       sequencePlayer1.push($square.id);
       console.log(sequencePlayer1);
+      flagTurn = true;
+      blockInput();
+      manageRound();
     }
   })
 }
+
+function turnPlayer2() {
+  $squares.forEach(function ($square) {
+    $square.onclick = function () {
+      sequencePlayer2.push($square.id);
+      console.log(sequencePlayer2);
+      flagTurn = false;
+      blockInput();
+      manageRound();
+    }
+  })
+}
+
+
 
 function hideElement($element) {
   $element.classList.remove("d-flex");
