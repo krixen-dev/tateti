26a9f6fc6731deb6999a7b97a3188e226f2a2543
Antonio <krixen.dev@gmail.com>
2023-09-18 23:52:23 -0300

    agrega lógica turno player1
    se agregó función que registra los clicks del player1 y lo guarda en un array

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const btnNext = document.getElementById("btn-next");
 const btnPlayGame = document.getElementById("btn-play-game");
+const player1 = document.getElementById("form__name_player1");
+const player2 = document.getElementById("form__name_player2");
+const panel = document.getElementById("panel");
+const $squares = panel.querySelectorAll(".square");
+let sequencePlayer1 = [];
+let sequencePlayer2 = [];
 
 btnNext.onclick = function () {
   hideElement(document.getElementById("form__form1"));
@@ -9,11 +15,19 @@ btnNext.onclick = function () {
 btnPlayGame.onclick = function () {
   hideElement(document.getElementById("form"));
   showElement(document.getElementById("game"));
+  turnPlayer1();
 }
 
 
 
-
+function turnPlayer1() {
+  $squares.forEach(function ($square) {
+    $square.onclick = function () {
+      sequencePlayer1.push($square.id);
+      console.log(sequencePlayer1);
+    }
+  })
+}
 
 function hideElement($element) {
   $element.classList.remove("d-flex");
@@ -24,3 +38,11 @@ function showElement($element) {
   $element.classList.remove("d-none");
   $element.classList.add("d-flex");
 }
+
+function blockInput() {
+  $squares.forEach(function ($square) {
+    $square.onclick = function () {
+    }
+  })
+}
+
