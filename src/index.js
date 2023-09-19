18d1208d1cbbe3f4f7e6828668bfab3edc792f9d
Antonio <krixen.dev@gmail.com>
2023-09-19 16:26:50 -0300

    agrega verificación
    se agregó la verificación de los cuadros que elgen los players, despúes de la segunda ronda empieza a verificar si cumplen con la secuencia necesaria para ganar el juego, es decir si forman el ta.te.ti

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const player1 = document.getElementById("form__name_player1");
 const player2 = document.getElementById("form__name_player2");
 const panel = document.getElementById("panel");
 const $squares = panel.querySelectorAll(".square");
-let flagTurn = false;
+let flag = false;
 let sequencePlayer1 = [];
 let sequencePlayer2 = [];
 
@@ -19,12 +19,8 @@ btnPlayGame.onclick = function () {
   manageRound();
 }
 
-
-
-
-
 function manageRound() {
-  if (!flagTurn) {
+  if (!flag) {
     turnPlayer1();
     console.log("turno player1");
   }else{
@@ -38,8 +34,9 @@ function turnPlayer1() {
     $square.onclick = function () {
       sequencePlayer1.push($square.id);
       paint($square, "primary");
+      verifyWinningSequence(sequencePlayer1);
       console.log(sequencePlayer1);
-      flagTurn = true;
+      flag = true;
       blockInput();
       manageRound();
     }
@@ -51,15 +48,14 @@ function turnPlayer2() {
     $square.onclick = function () {
       sequencePlayer2.push($square.id);
       paint($square, "danger");
+      verifyWinningSequence(sequencePlayer2);
       console.log(sequencePlayer2);
-      flagTurn = false;
+      flag = false;
       blockInput();
       manageRound();
     }
   })
 }
-
-
 
 function hideElement($element) {
   $element.classList.remove("d-flex");
@@ -80,5 +76,27 @@ function blockInput() {
 
 function paint($element, color) {
   $element.classList.add("bg-"+ color);
+}
+
+function verifyWinningSequence(sequence) {
+  if (sequence.length > 2) {
+    console.log("empieza a verificar");
+    if (sequenceWinner(sequence,1,2,3)|| 
+        sequenceWinner(sequence,4,5,6)|| 
+        sequenceWinner(sequence,7,8,9)||
+        sequenceWinner(sequence,1,4,7)||
+        sequenceWinner(sequence,2,5,8)||
+        sequenceWinner(sequence,3,6,9)||
+        sequenceWinner(sequence,7,5,3)||
+        sequenceWinner(sequence,9,5,1)){
+      return alert("ganaste!")
+    }
+  }
+}
+
+function sequenceWinner(sequence, num1, num2, num3) {
+  if (sequence.includes("square-"+num1) && sequence.includes("square-"+num2) && sequence.includes("square-"+num3) ) {
+    return true;
+  }
 }
 
